aaff2a093bd81947dab979a41decf1a77c33093f
Deborah3321 <nchabelengdeborah18@gmail.com>
2023-12-14 01:45:44 -0800

    monty

--- main.c
+++ main.c
@@ -1,34 +1,83 @@
 #include "monty.h"
-
-glob_t glob;
+stack_t *head = NULL;
 
 /**
- * main - entry point for the monty program
- * @argc: number of command line arguments
- * @argv: array of command line argument strings
- *
- * Return: 0 on success, non-zero on failure
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	stack_t *stack = NULL;
-
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
 
-	glob.file = fopen(argv[1], "r");
-	if (glob.file == NULL)
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		tmp = head;
+		head = head->next;
+		free(tmp);
 	}
+}
 
-	execute_file(&stack);
-	fclose(glob.file);
-	free(glob.line);
-	free_stack(stack);
-	exit(EXIT_SUCCESS);
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
 }
